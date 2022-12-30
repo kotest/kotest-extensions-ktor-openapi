dd3a8513f1cb37bf5116056d434f5cd6057c4251
sksamuel <sam@sksamuel.com>
2022-12-30 17:31:19 -0600

    Multi parameters

--- src/test/kotlin/io/kotest/extensions/ktor/openapi/OpenApiTest.kt
+++ src/test/kotlin/io/kotest/extensions/ktor/openapi/OpenApiTest.kt
@@ -2,11 +2,13 @@ package io.kotest.extensions.ktor.openapi
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import io.ktor.client.request.delete
 import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
 import io.ktor.server.response.respond
+import io.ktor.server.routing.delete
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
@@ -29,11 +31,13 @@ class OpenApiTest : FunSpec() {
                   }
                }
                get("/with/param/{foo}") { call.respond(HttpStatusCode.OK) }
+               delete("/with/param1/{foo}/even/{param2}") { call.respond(HttpStatusCode.OK) }
                post("/bar2") { call.respond(HttpStatusCode.OK) }
             }
             client.get("/internal/foo1")
             client.post("/bar2").status shouldBe HttpStatusCode.OK
             client.get("/with/param/abc").status shouldBe HttpStatusCode.OK
+            client.delete("/with/param1/foo/even/param2").status shouldBe HttpStatusCode.OK
          }
       }
    }
