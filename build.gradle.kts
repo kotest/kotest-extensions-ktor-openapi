edd3765f3e815ba011cdc93a783c12a9b81b1dfe
sksamuel <sam@sksamuel.com>
2023-03-12 17:52:00 -0500

    Bumped deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,13 +19,13 @@ repositories {
 }
 
 dependencies {
-   implementation("io.ktor:ktor-server-core:2.2.1")
-   implementation("io.ktor:ktor-server-auth:2.2.1")
-   implementation("io.swagger.parser.v3:swagger-parser:2.1.8")
-   testImplementation("io.ktor:ktor-client-apache:2.2.1")
+   implementation("io.ktor:ktor-server-core:2.2.4")
+   implementation("io.ktor:ktor-server-auth:2.2.4")
+   implementation("io.swagger.parser.v3:swagger-parser:2.1.12")
+   testImplementation("io.ktor:ktor-client-apache:2.2.4")
    testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
    testImplementation("io.kotest:kotest-assertions-core:5.5.4")
-   testImplementation("io.ktor:ktor-server-test-host:2.2.1")
+   testImplementation("io.ktor:ktor-server-test-host:2.2.4")
 }
 
 tasks.test {
